4ded8264678414234495d072065b55a9d83fc678
ConceiçãoAmaral <mc_amaral@uol.com.br>
2021-04-24 15:11:36 -0300

    "Parte1 Classe Pessoa"

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,12 +2,17 @@ package main.kotlin.one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Jether"
-    var cpf: String = "12.436.789-10"
+    var cpf: String = "123.436.789-10"
+    private set
+
 }
 
 fun main() {
     val jether =  Pessoa()
 
+    println(jether)
     println(jether.nome)
     println(jether.cpf)
+
+
 }
